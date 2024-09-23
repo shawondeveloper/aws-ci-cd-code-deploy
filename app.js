2523314c131d3c25bc010a8e3181105123f010a5
Shajedul Islam Shawon <54211870+shawondeveloper@users.noreply.github.com>
2024-09-23 21:51:31 +0600

    changed app.js

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-console.log('Hi Dear,')
+const express = require('express')
 
+const app = express()
+
+app.use('/',(req, res)=>{
+    res.send({message:"Hellow from aws node app"})
+})
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log('Server Started from '+ PORT)
+})
