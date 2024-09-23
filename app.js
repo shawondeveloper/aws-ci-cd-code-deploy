724bab27092894cacca1355fdaff3b9617e23832
Shajedul Islam Shawon <54211870+shawondeveloper@users.noreply.github.com>
2024-09-23 21:57:41 +0600

    added express

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-console.log('Hi Dear,')
+const express = require("express");
 
+const app = express();
+
+app.use('/',(req, res) => {
+    res.status(200).json({message:"Hello from CICD aws github"})
+})
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server Stated at ${PORT}`);
+});
